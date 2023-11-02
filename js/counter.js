b816c0c10a8d9cc2c7899536359cb1117262dc27
Viettran1995 <144688233+Viettran1995@users.noreply.github.com>
2023-11-02 10:59:45 +0700

    Add files via upload

---
+++ js/counter.js
@@ -0,0 +1,2 @@
+// JavaScript Document
+$('.counter').countUp();
---
+++ js/type-control.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+var typed = new Typed('.element', {
+strings: ["Photographer.", "Videographer.","Video Editor.","Web Designer."],
+typeSpeed:40,
+backSpeed:40,
+     loop:true,
+   });
